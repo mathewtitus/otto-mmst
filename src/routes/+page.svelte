894fde204636cdeab2076627772f8b0f01a9be28
Mathew Titus <litsos.titus@gmail.com>
2024-05-17 21:20:54 -0700

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+  import Entry from "./Entry.svelte";
+  import { onMount } from 'svelte';
+  
+  let p, q, r;
+  // let setVars;
+
+  onMount(() => {
+    function setVars() {
+      p = document.getElementById("p-entry").value;
+      q = document.getElementById("q-entry").value;
+      r = document.getElementById("r-entry").value;
+    };
+
+    setVars();
+    document.addEventListener("input", setVars);
+  })
+
+</script>
+
+<h1>Mean Minimal Spanning Tree Calculator</h1>
+
+<row style="display: flex; flex-flow: row; justify-content: space-around;">
+<column>
+<Entry name="p"/>
+<Entry name="q"/>
+<Entry name="r"/>
+</column>
+<div>V = </div>
+<column>
+0 {p} {q} {r}<br>{p} 0 {r} {q}<br>{q} {r} 0 {p}<br>{r} {q} {p} 0
+</column>
+</row>
+
---
+++ src/routes/Entry.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let name = "";
+  // export let value = document.getElementById("{name}-entry")[0].value;
+  // {}
+</script>
+
+<div class="preamble">
+  <span>
+    {name} = 
+  </span>
+  <input id="{name}-entry" type="number" value="1">
+</div>
+
+<style>
+  input {
+    width:40px;
+  }
+  .preamble {
+    display: flex;
+    flex-flow: row;
+    justify-content: space-between;
+    width: 74px;
+  }
+</style>
